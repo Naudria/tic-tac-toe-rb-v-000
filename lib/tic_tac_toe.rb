7a4d7d457f37fc632ae367a7bf07a1da9247ac1b
Rima Jean <rima.jean@gmail.com>
2018-02-11 23:08:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,12 @@
+
+require_relative '../lib/tic_tac_toe'
+
+puts "Welcome to Tic Tac Toe!"
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  play(board)
+
+  _______________________________________________________
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
